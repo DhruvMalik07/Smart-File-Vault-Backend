ad619ac6dd710a9ecc0e7421c6d2ca25568891d2
Dhruv <dhruvmalikjat85@gmail.com>
2025-06-12 17:20:55 +0530

    Fixing CORS

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: ['https://smart-file-vault-frontend.onrender.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(express.json());
